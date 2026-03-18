c2670debfc25376d6d1856bf7141bf523578f695
Emanuele Vaccaro <evaccaro.001@gmail.com>
2026-03-18 09:18:43 +0100

    add file to repo

---
+++ assets/js/script.js
@@ -0,0 +1,3 @@
+//1.Crea una funzione che controlli due numeri interi. Ritorna 'true' se uno dei due è 50 o se la somma dei due è 50
+
+//2. Crea una funzione che rimuova il carattere ad una specifica posizione da un stringa. Passa la stringa e la posizione come parametri e ritorna la strina modificata
