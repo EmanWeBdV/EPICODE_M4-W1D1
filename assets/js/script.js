3d1e7d52e1aa8245d21a18478d1497b5de3409c4
Emanuele Vaccaro <evaccaro.001@gmail.com>
2026-03-19 16:51:19 +0100

    end

--- assets/js/script.js
+++ assets/js/script.js
@@ -207,6 +207,17 @@ console.log(reversNumber(342))
 //14.Extra - 6. Scrivi una funzione che accetti un numero positivo X come parametro. La funzione dovrebbe stampare a console una “scala” creata con il carattere “#” e avente X scalini.
 
 
+function scaleOfHash(num){
+    let scaleHash = ""
+    for (let i = 0; i < num; i++){
+        scaleHash += "#"
+        console.log(scaleHash)
+    }
+}
+
+console.log(scaleOfHash(3))
+
+
 
 /*
 Es. 
@@ -228,6 +239,14 @@ X = 3
 
 //15.Extra - 7. Crea una funzione che, data una stringa come parametro, ritorni la stessa stringa, ma al contrario. Es. “Ciao” ****⇒ “oaiC”
 
+function reversText(str){
+    let arrStr = str.split("").reverse()
+    let returnString = arrStr.toString().replaceAll(",", "")
+    return returnString
+}
+
+console.log(reversText("Ciao"))
+
 //16.Extra - 8. Crea una funzione che accetti un array e un numero Y come parametro. Dividi l’array in sotto-array aventi lunghezza Y.
 /*
 Es. array: [1, 2, 3, 4], y: 2 ⇒ [[ 1, 2], [3, 4]] 
@@ -235,7 +254,34 @@ Es. array: [1, 2, 3, 4], y: 2 ⇒ [[ 1, 2], [3, 4]]
 array: [1, 2, 3, 4, 5], y: 4 ⇒ [[ 1, 2, 3, 4], [5]]
 */
 
+
+
+function splitNumber(arr, num){
+    let newArray = []
+    for(let i = 0; i < arr.length; i += num){
+        let newSubArray = arr.slice(i, i + num)
+        newArray.push(newSubArray)
+    }
+    return newArray
+}
+
+let groupNum = [1, 2, 3, 4,]
+console.log(splitNumber(groupNum, 2))
+
+
 //17.Extra - 9. Scrivi una funzione che accetti un numero positivo X come parametro. La funzione dovrebbe stampare a console una “piramide” create con il carattere “#” e avente X strati.
+
+
+function piramideOfHash(num){
+    let piramideOfHash = ""
+    for (let i = 0; i < num; i++){
+        piramideOfHash += "#"
+        console.log(piramideOfHash)
+    }
+}
+
+(piramideOfHash(4))
+
 
 /*
 Es. 
@@ -250,6 +296,7 @@ X = 3
 */
 
 //18.Extra - 10. Scrivi una funzione che accetti un intero N e ritorni una matrice a spirale NxN:
+
 /*
 Es. N = 2
 
@@ -263,3 +310,6 @@ N = 4
 
 [[1, 2, 3, 4],[12, 13, 14, 5],[11, 16, 15, 6],[10, 9, 8, 7]]
 */
+
+
+//Devo ancora migliorare molto sotto js... quindi preferisco consegnarlo senza gli ultimi due esercizi
